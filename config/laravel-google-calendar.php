c98fe2379b1247925ed50d47a669fca7af6c6c1e
ALEX-PC\Alex <aleexsoarees@gmail.com>
2016-11-18 20:33:06 -0300

    autoload

--- config/laravel-google-calendar.php
+++ config/laravel-google-calendar.php
@@ -5,7 +5,7 @@ return [
     /**
      * Path to a json file containing the credentials of a Google Service account.
      */
-    'client_secret_json' => storage_path('app/laravel-google-calendar/VCcon-650529427af7.json'),
+    'client_secret_json' => storage_path('app/laravel-google-calendar/VCcon-f793ec14d065.json'),
 
     /**
      *  The id of the Google Calendar that will be used by default.
