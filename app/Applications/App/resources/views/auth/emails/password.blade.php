d2efe5b6ce4480bbaa908f110f7b7f2e84ff135c
ALEX-PC\Alex <aleexsoarees@gmail.com>
2016-11-21 17:00:13 -0300

    Ajustes layout!

--- app/Applications/App/resources/views/auth/emails/password.blade.php
+++ app/Applications/App/resources/views/auth/emails/password.blade.php
@@ -1,1 +1,1 @@
-Click here to reset your password: <a href="{{ $link = url('password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}"> {{ $link }} </a>
+Clique aqui para resetar sua senha: <a href="{{ $link = url('password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}"> {{ $link }} </a>
